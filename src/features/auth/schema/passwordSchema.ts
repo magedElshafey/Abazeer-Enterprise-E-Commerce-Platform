37862222288b45eaeef8e3e7a82c6030da944b1c
magedElshafey <magedelshafey98@gmail.com>
2026-02-12 13:23:00 +0200

    updates

--- src/features/auth/schema/passwordSchema.ts
+++ src/features/auth/schema/passwordSchema.ts
@@ -3,12 +3,13 @@ import { z } from "zod";
 // ✅ يستخدم في login فقط
 export const singlePasswordSchema = z
   .string()
-  .min(8, "Password must be at least 8 characters long")
-  .regex(/[A-Z]/, "Password must contain at least one uppercase letter")
+  .min(8, "Password must be at least 8 characters long");
+/**
+ *  .regex(/[A-Z]/, "Password must contain at least one uppercase letter")
   .regex(/[a-z]/, "Password must contain at least one lowercase letter")
   .regex(/[0-9]/, "Password must contain at least one number")
-  .regex(/[@$!%*?&]/, "Password must contain at least one special character");
-
+  .regex(/[@$!%*?&]/, "Password must contain at least one special character")
+ */
 // ✅ يستخدم في register/reset password
 export const passwordWithConfirmSchema = z
   .object({
@@ -28,7 +29,9 @@ export const changePasswordSchema = z
   .object({
     current_password: z.string().min(1, "password is required"),
     new_password: singlePasswordSchema,
-    new_password_confirmation: z.string().min(1, "Please confirm your password"),
+    new_password_confirmation: z
+      .string()
+      .min(1, "Please confirm your password"),
   })
   .refine((data) => data.new_password === data.new_password_confirmation, {
     message: "Passwords do not match",
